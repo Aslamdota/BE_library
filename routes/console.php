ee650eb2639d1b477972359e2c9d98cc82c6c671
Sigit Hardianto <69493819+sihar7@users.noreply.github.com>
2025-05-28 21:28:06 +0700

    denda

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Configuration\Schedule;
+use App\Console\Commands\UpdateLoanFines;
+
+Schedule::command(UpdateLoanFines::class)->everyMinute();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
